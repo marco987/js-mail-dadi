cb97cafc7d1ff41e8c0fe50ab8d6348dcc2126a6
marco987 <pacemarco87@gmail.com>
2019-07-03 09:39:47 +0200

    Primo tentativo

--- mail/script.js
+++ mail/script.js
@@ -4,15 +4,23 @@
 // e stampa un messaggio appropriato
 
 // Dichiarazione variabili
-
+var elenco_email, utente_email;
+var invitato = document.getElementById("invitato")
 
 // Elenco email degli utenti invitati
-
+elenco_email = ["invitato1@gmail.com", "invitato2@gmail.com", "invitato3@gmail.com", "invitato4@gmail.com", "invitato5@gmail.com"];
 
 // L'utente inserisce sua email
-
+utente_email = prompt("Qual è la tua email?");
 
 // Verifica email utente
-
+for (var i = 0; i < elenco_email.length; i++) {
+  if (i == utente_email) {
+    invitato.innerHTML = "Sei nella lista degli invitati! Ti aspettiamo.";
+  }
+  else {
+    invitato.innerHTML = "Spiacente, non sei nella lista degli invitati.";
+  }
+}
 
 // Messaggio di invito
