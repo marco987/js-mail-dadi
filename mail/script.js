ea35b041511c660d0a4b2256f5d214a9e14c159a
marco987 <pacemarco87@gmail.com>
2019-07-03 09:19:34 +0200

    Struttura e commenti base

---
+++ mail/script.js
@@ -0,0 +1,18 @@
+// TRACCIA
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere
+// e stampa un messaggio appropriato
+
+// Dichiarazione variabili
+
+
+// Elenco email degli utenti invitati
+
+
+// L'utente inserisce sua email
+
+
+// Verifica email utente
+
+
+// Messaggio di invito
